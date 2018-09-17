05fc0070d377d62321765ec1e94a61c21098e384
Gilber Moreno <6ilberM@gmail.com>
2018-09-17 18:36:31 +1200

    Libraries and dependencies linked.

---
+++ SeasonOfMists/SeasonOfMists/main.cpp
@@ -0,0 +1,28 @@
+/*
+ _____                              _____  __  ___  ___          _
+/  ___|                            |  _  |/ _| |  \/  |         | |
+\ `--.  ___  __ _ ___  ___  _ __   | | | | |_  | .  . |_   _ ___| |_ ___
+ `--. \/ _ \/ _` / __|/ _ \| '_ \  | | | |  _| | |\/| | | | / __| __/ __|
+/\__/ /  __/ (_| \__ \ (_) | | | | \ \_/ / |   | |  | | |_| \__ \ |_\__ \
+\____/ \___|\__,_|___/\___/|_| |_|  \___/|_|   \_|  |_/\__, |___/\__|___/
+														__/ |
+													   |___/
+*/
+
+#include <btBulletDynamicsCommon.h>
+#include <glew.h>
+#include <GLFW/glfw3.h>
+#include<iostream>
+
+#include <vld.h>
+using namespace std;
+
+btDiscreteDynamicsWorld* m_world;
+
+int main()
+{
+	m_world;
+	int x;
+	cin >> x;
+	return 0;
+}
