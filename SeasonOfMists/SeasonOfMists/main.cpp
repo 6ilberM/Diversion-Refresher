0acdaf75bcacca761425340f650f0e0b42b2a47c
Gilber Moreno <6ilberM@gmail.com>
2018-09-20 12:38:38 +1200

    Got a window!

--- SeasonOfMists/SeasonOfMists/main.cpp
+++ SeasonOfMists/SeasonOfMists/main.cpp
@@ -15,14 +15,66 @@
 #include<iostream>
 
 #include <vld.h>
-using namespace std;
+//My includes
 
-btDiscreteDynamicsWorld* m_world;
+using namespace std;
+GLFWwindow* Main_Window;
+GLuint Default_Shader;
+
+//btDiscreteDynamicsWorld* m_world;
+
+//In case of Exit
+void KeyboardInput(GLFWwindow *_window, int _key, int _scancode, int _action, int mods)
+{
+	if (_key == GLFW_KEY_ESCAPE && _action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(_window, true);
+	}
+
+}
+
+int init()
+{
+	if (!glfwInit())
+	{
+		//init failed
+		return -1;
+	}
+
+	Main_Window = glfwCreateWindow(1280, 720, "Season Of Mists", NULL, NULL);
+
+	if (!Main_Window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(Main_Window);
+
+	GLenum GlewChk = glewInit();
+	if (GLEW_OK != GlewChk)
+	{
+		return -1;
+		glfwTerminate();
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	glfwSetKeyCallback(Main_Window, KeyboardInput);
+
+}
+
 
 int main()
 {
-	m_world;
-	int x;
-	cin >> x;
-	return 0;
+	init();
+	while (!glfwWindowShouldClose(Main_Window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+		glfwSwapBuffers(Main_Window);
+		glfwPollEvents();
+	}
+	//you need this to avoid memory leaks 6 of them to be precise
+	glfwTerminate();
 }
